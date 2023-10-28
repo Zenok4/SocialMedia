0972c8df545fd7c96f9227d3155396a7166c6b0d
Zenok4 <dungk4.camlo@gmail.com>
2023-10-28 21:58:14 +0700

    fix mini bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,45 @@
-import Head from "next/head";
-import "./globals.css";
-import App, { AppContext, AppInitialProps, AppProps } from "next/app";
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useState } from "react";
-import TimeAgo from 'javascript-time-ago'
+  import Head from "next/head";
+  import "./globals.css";
+  import App, { AppContext, AppInitialProps, AppProps } from "next/app";
+  import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+  import { SessionContextProvider } from "@supabase/auth-helpers-react";
+  import { useState } from "react";
+  import TimeAgo from 'javascript-time-ago'
 
-type AppOwnProps = { example: string };
+  type AppOwnProps = { example: string };
 
 
-import en from 'javascript-time-ago/locale/en.json'
+  import en from 'javascript-time-ago/locale/en.json'
 
-TimeAgo.addDefaultLocale(en)
-TimeAgo.addLocale(en)
+  TimeAgo.addDefaultLocale(en)
+  TimeAgo.addLocale(en)
 
-export default function MyApp({
-  Component,
-  pageProps,
-  example,
-}: AppProps & AppOwnProps) {
-  const [supabaseClient] = useState(() => createPagesBrowserClient());
-  return (
-    <>
-      <Head>
-        <title>Social Meida</title>
-        <link rel="icon" href="/img/favicon.ico" sizes="any" />
-      </Head>
-      <SessionContextProvider
-        supabaseClient={supabaseClient}
-        initialSession={pageProps.initialSession}
-      >
-        <Component {...pageProps} />
-      </SessionContextProvider>
-    </>
-  );
-}
+  export default function MyApp({
+    Component,
+    pageProps,
+    example,
+  }: AppProps & AppOwnProps) {
+    const [supabaseClient] = useState(() => createPagesBrowserClient());
+    return (
+      <>
+        <Head>
+          <title>Social Meida</title>
+          <link rel="icon" href="/img/favicon.ico" sizes="any" />
+        </Head>
+        <SessionContextProvider
+          supabaseClient={supabaseClient}
+          initialSession={pageProps.initialSession}
+        >
+          <Component {...pageProps} />
+        </SessionContextProvider>
+      </>
+    );
+  }
 
-MyApp.getInitialProps = async (
-  context: AppContext
-): Promise<AppOwnProps & AppInitialProps> => {
-  const ctx = await App.getInitialProps(context);
+  MyApp.getInitialProps = async (
+    context: AppContext
+  ): Promise<AppOwnProps & AppInitialProps> => {
+    const ctx = await App.getInitialProps(context);
 
-  return { ...ctx, example: "data" };
-};
+    return { ...ctx, example: "data" };
+  };
